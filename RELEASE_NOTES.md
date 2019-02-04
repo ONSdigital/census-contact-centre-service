b5a32dce62f1c0030c5ab75f221e9e454465acb2
Alin Dragota <alindragota@C02S60HHG8WM.local>
2019-02-04 09:38:07 +0000

    CDTT-736 - Created skeleton for the Contact Center Service

---
+++ RELEASE_NOTES.md
@@ -0,0 +1,12 @@
+Contact Center Service
+============
+Version 1.0.0
+
+Changes
+-------
+Setting up the skeleton project for Contact Center Service
+
+Known Issues
+------------
+None.
+
---
+++ src/main/java/uk/gov/ons/ctp/response/contactcentersvc/ContactCenterSvcApplication.java
@@ -0,0 +1,10 @@
+package uk.gov.ons.ctp.response.contactcentersvc;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/** The 'main' entry point for the ContactCenterSvc SpringBoot Application. */
+@SpringBootApplication
+public class ContactCenterSvcApplication {
+
+
+}
