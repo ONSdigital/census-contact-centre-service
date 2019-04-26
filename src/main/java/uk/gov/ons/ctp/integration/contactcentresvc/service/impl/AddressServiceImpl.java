0c4dfd2868bf2f135125c8d013c54c445bd2e140
Peter Bochel <Peter.Bochel@ext.ons.gov.uk>
2019-04-26 14:26:31 +0100

    Now using the results total as returned by Address Index

--- src/main/java/uk/gov/ons/ctp/integration/contactcentresvc/service/impl/AddressServiceImpl.java
+++ src/main/java/uk/gov/ons/ctp/integration/contactcentresvc/service/impl/AddressServiceImpl.java
@@ -86,7 +86,7 @@ public class AddressServiceImpl implements AddressService {
     AddressQueryResponseDTO queryResponse = new AddressQueryResponseDTO();
     queryResponse.setDataVersion(addressIndexResponse.getDataVersion());
     queryResponse.setAddresses(summarisedAddresses);
-    queryResponse.setTotal(summarisedAddresses.size());
+    queryResponse.setTotal(addressIndexResponse.getResponse().getTotal());
 
     return queryResponse;
   }
