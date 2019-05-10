6d4275db277a1d144897f18a21fa31156001a030
Phil Whiles <phil.whiles@gmail.com>
2019-05-10 10:17:02 +0100

    fix inadvertent typo

--- scripts/docker_push.sh
+++ scripts/docker_push.sh
@@ -4,7 +4,7 @@ set -e
 
 echo $GCLOUD_SERVICE_KEY | base64 -d | docker login -u _json_key --password-stdin https://eu.gcr.io
 
-export VERSION=`mvn help:evaluate -Dexpressioet.version -q -DforceStdout`
+export VERSION=`mvn help:evaluate -Dexpression=project.version -q -DforceStdout`
 export VERSIONTAG=`if [ "$TRAVIS_PULL_REQUEST_BRANCH" == "" ]; then echo $VERSION; else echo $VERSION"-"$TRAVIS_PULL_REQUEST_BRANCH; fi`
 export RELEASETAG=`if [ "$TRAVIS_PULL_REQUEST_BRANCH" == "" ]; then echo "latest"; fi`
 
