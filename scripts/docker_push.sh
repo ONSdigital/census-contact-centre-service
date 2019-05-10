088633617930261b6735abc46e1694d3ea624dd2
Phil Whiles <phil.whiles@gmail.com>
2019-05-10 10:13:30 +0100

    travis docker push tweak

--- scripts/docker_push.sh
+++ scripts/docker_push.sh
@@ -3,13 +3,17 @@
 set -e
 
 echo $GCLOUD_SERVICE_KEY | base64 -d | docker login -u _json_key --password-stdin https://eu.gcr.io
-export VERSION=`mvn help:evaluate -Dexpression=project.version -q -DforceStdout`
-export VERSIONTAG=`if [ "$TRAVIS_PULL_REQUEST_BRANCH" == "" ]; then echo $VERSION; else echo $VERSION"-"$TRAVIS_PULL_REQUEST_BRANCH; fi`
-export PURPOSETAG=`if [ "$TRAVIS_PULL_REQUEST_BRANCH" == "" ]; then echo "latest"; else echo "snapshot"; fi`
 
-echo "Building with tags [$VERSIONTAG $PURPOSETAG]"
+export VERSION=`mvn help:evaluate -Dexpressioet.version -q -DforceStdout`
+export VERSIONTAG=`if [ "$TRAVIS_PULL_REQUEST_BRANCH" == "" ]; then echo $VERSION; else echo $VERSION"-"$TRAVIS_PULL_REQUEST_BRANCH; fi`
+export RELEASETAG=`if [ "$TRAVIS_PULL_REQUEST_BRANCH" == "" ]; then echo "latest"; fi`
+
+echo "Building with tags [$VERSIONTAG $RELEASETAG]"
 
 docker build -t eu.gcr.io/census-int-ci/census-contact-centre-service:$VERSIONTAG .
 docker push eu.gcr.io/census-int-ci/census-contact-centre-service:$VERSIONTAG
-docker tag eu.gcr.io/census-int-ci/census-contact-centre-service:$VERSIONTAG eu.gcr.io/census-int-ci/census-contact-centre-service:$PURPOSETAG
-docker push eu.gcr.io/census-int-ci/census-contact-centre-service:$PURPOSETAG
+if [ ! -z "$RELEASETAG" ]; then
+  docker tag eu.gcr.io/census-int-ci/census-contact-centre-service:$VERSIONTAG eu.gcr.io/census-int-ci/census-contact-centre-service:$RELEASETAG
+  docker push eu.gcr.io/census-int-ci/census-contact-centre-service:$RELEASETAG
+fi
+
