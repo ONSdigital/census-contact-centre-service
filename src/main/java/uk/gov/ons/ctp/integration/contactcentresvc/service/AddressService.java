7e97da96e4b03a8569c317134a771ca3039fe5b4
Martin Humphrey <Martin.Humphrey@ons.gov.uk>
2020-02-07 10:52:38 +0000

    CR-689 Requested to remove redundant update endpoint and associated svc

--- src/main/java/uk/gov/ons/ctp/integration/contactcentresvc/service/AddressService.java
+++ src/main/java/uk/gov/ons/ctp/integration/contactcentresvc/service/AddressService.java
@@ -1,11 +1,8 @@
 package uk.gov.ons.ctp.integration.contactcentresvc.service;
 
-import uk.gov.ons.ctp.common.time.DateTimeUtil;
 import uk.gov.ons.ctp.integration.contactcentresvc.representation.AddressQueryRequestDTO;
 import uk.gov.ons.ctp.integration.contactcentresvc.representation.AddressQueryResponseDTO;
-import uk.gov.ons.ctp.integration.contactcentresvc.representation.AddressUpdateRequestDTO;
 import uk.gov.ons.ctp.integration.contactcentresvc.representation.PostcodeQueryRequestDTO;
-import uk.gov.ons.ctp.integration.contactcentresvc.representation.ResponseDTO;
 
 /** Service responsible for dealing with address related functionality */
 public interface AddressService {
@@ -33,12 +30,4 @@ public interface AddressService {
    * @return result object as for other searches but with only ever 0 or 1 result
    */
   public AddressQueryResponseDTO uprnQuery(Long uprn);
-
-  // TODO
-  public default ResponseDTO addressChange(AddressUpdateRequestDTO addressUpdateRequestDTO) {
-    ResponseDTO fakeResponse = new ResponseDTO();
-    fakeResponse.setId("8437625585067");
-    fakeResponse.setDateTime(DateTimeUtil.nowUTC());
-    return fakeResponse;
-  }
 }
