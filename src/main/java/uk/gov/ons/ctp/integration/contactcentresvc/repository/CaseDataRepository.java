c6f94a6035bf4501a5a2c7bc77819c66b7620ae4
Martin Humphrey <Martin.Humphrey@ons.gov.uk>
2020-02-07 10:27:46 +0000

    CR-689 Add Firestore support
    Simple copy of RH Service functionality

---
+++ src/main/java/uk/gov/ons/ctp/integration/contactcentresvc/repository/CaseDataRepository.java
@@ -0,0 +1,15 @@
+package uk.gov.ons.ctp.integration.contactcentresvc.repository;
+
+import java.util.Optional;
+import uk.gov.ons.ctp.common.error.CTPException;
+import uk.gov.ons.ctp.common.event.model.CollectionCase;
+
+/** Repository for Case Data */
+public interface CaseDataRepository {
+
+  /** Write a Case */
+  void writeCollectionCase(CollectionCase collectionCase) throws CTPException;
+
+  /** Read a Case */
+  Optional<CollectionCase> readCollectionCase(String caseId) throws CTPException;
+}
