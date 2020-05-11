d32f980a7b751f458c99c6ce5dc9ebee062624f2
Rob Catling <github@rpc-soft.com>
2020-05-11 15:21:42 +0100

    CR-946 changes for Spring Boot 2.3.0 - update dependencies too

--- src/test/java/uk/gov/ons/ctp/integration/contactcentresvc/service/impl/AddressServiceImplTest.java
+++ src/test/java/uk/gov/ons/ctp/integration/contactcentresvc/service/impl/AddressServiceImplTest.java
@@ -4,6 +4,7 @@ import static org.hamcrest.CoreMatchers.startsWith;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
+
 import java.util.ArrayList;
 import org.junit.Before;
 import org.junit.Test;
