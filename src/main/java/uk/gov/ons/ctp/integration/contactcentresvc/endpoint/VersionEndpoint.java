17b5713681c83c487bb04621c15335fb30816d07
Rob Catling <github@rpc-soft.com>
2020-06-12 09:16:23 +0100

    CR-000 automatically get swagger version from CCAPI swagger current

--- src/main/java/uk/gov/ons/ctp/integration/contactcentresvc/endpoint/VersionEndpoint.java
+++ src/main/java/uk/gov/ons/ctp/integration/contactcentresvc/endpoint/VersionEndpoint.java
@@ -2,9 +2,15 @@ package uk.gov.ons.ctp.integration.contactcentresvc.endpoint;
 
 import com.godaddy.logging.Logger;
 import com.godaddy.logging.LoggerFactory;
+import java.io.IOException;
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.yaml.snakeyaml.Yaml;
 import uk.gov.ons.ctp.common.endpoint.CTPEndpoint;
 import uk.gov.ons.ctp.integration.contactcentresvc.representation.VersionResponseDTO;
 
@@ -12,8 +18,14 @@ import uk.gov.ons.ctp.integration.contactcentresvc.representation.VersionRespons
 @RestController
 @RequestMapping(value = "/", produces = "application/json")
 public final class VersionEndpoint implements CTPEndpoint {
-  public static final String CC_API_VERSION = "5.10.4";
   private static final Logger log = LoggerFactory.getLogger(VersionEndpoint.class);
+
+  private ResourceLoader resourceLoader;
+
+  @Autowired
+  public VersionEndpoint(ResourceLoader resourceLoader) {
+    this.resourceLoader = resourceLoader;
+  }
 
   /**
    * the GET endpoint to get contact centre Details
@@ -24,7 +36,24 @@ public final class VersionEndpoint implements CTPEndpoint {
   public VersionResponseDTO getVersion() {
     log.info("Entering GET getVersion");
     VersionResponseDTO fakeVersion =
-        VersionResponseDTO.builder().apiVersion(CC_API_VERSION).build();
+        VersionResponseDTO.builder().apiVersion(swaggerVersion()).build();
     return fakeVersion;
+  }
+
+  @SuppressWarnings("unchecked")
+  private String swaggerVersion() {
+    String ver = "UNKNOWN";
+    Resource resource = resourceLoader.getResource("classpath:swagger-current.yml");
+
+    Yaml yaml = new Yaml();
+    try {
+      Map<String, Object> obj = yaml.load(resource.getInputStream());
+      Map<String, Object> info = (Map<String, Object>) obj.get("info");
+      ver = info.get("version").toString();
+      ver = ver.replace("-oas3", "");
+    } catch (IOException e) {
+      log.error("Cannot determine Swagger version", e);
+    }
+    return ver;
   }
 }
