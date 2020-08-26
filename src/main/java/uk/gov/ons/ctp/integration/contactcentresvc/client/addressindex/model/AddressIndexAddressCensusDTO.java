52c8f196391e21e4c2d8f202ea8a151792a86a92
Peter Bochel <Peter.Bochel@ext.ons.gov.uk>
2020-08-26 16:15:12 +0100

    CR-1185 Moved some fields to new census object

---
+++ src/main/java/uk/gov/ons/ctp/integration/contactcentresvc/client/addressindex/model/AddressIndexAddressCensusDTO.java
@@ -0,0 +1,14 @@
+package uk.gov.ons.ctp.integration.contactcentresvc.client.addressindex.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class AddressIndexAddressCensusDTO {
+  private String countryCode;
+
+  private String censusAddressType;
+
+  private String censusEstabType;
+}
