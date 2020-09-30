e23e5116cc8d1010d3e2203ede1d035275ccf35d
cookel2 <eleanor.cook@ons.gov.uk>
2020-09-30 16:22:00 +0100

    CR-744 create JUnit test getCcsCaseByPostcode_PostcodeInListButNotInRM

--- src/test/java/uk/gov/ons/ctp/integration/contactcentresvc/endpoint/CaseEndpointGetCcsCaseTest.java
+++ src/test/java/uk/gov/ons/ctp/integration/contactcentresvc/endpoint/CaseEndpointGetCcsCaseTest.java
@@ -33,8 +33,8 @@ import uk.gov.ons.ctp.integration.contactcentresvc.representation.CaseEventDTO;
 import uk.gov.ons.ctp.integration.contactcentresvc.service.CaseService;
 
 /**
- * Contact Centre Data Endpoint Unit tests. This class tests the get case endpoints, covering gets
- * based on uuid, ref and uprn.
+ * Contact Centre Data Endpoint Unit tests. This class tests the get ccs case endpoints, covering
+ * get ccs case by postcode.
  */
 public final class CaseEndpointGetCcsCaseTest {
 
@@ -59,8 +59,6 @@ public final class CaseEndpointGetCcsCaseTest {
   @Mock CaseService caseService;
 
   private MockMvc mockMvc;
-
-  private UUID uuid = UUID.randomUUID();
 
   /**
    * Set up of tests
@@ -91,11 +89,15 @@ public final class CaseEndpointGetCcsCaseTest {
     verifyStructureOfMultiResultsActions(actions);
   }
 
-  // @Test
-  // public void getCaseById_BadId() throws Exception {
-  // ResultActions actions = mockMvc.perform(getJson("/cases/123456789"));
-  // actions.andExpect(status().isBadRequest());
-  // }
+  @Test
+  public void getCcsCaseByPostcode_PostcodeInListButNotInRM() throws Exception {
+    ResponseStatusException ex =
+        new ResponseStatusException(
+            HttpStatus.NOT_FOUND, "Not Found", new HttpClientErrorException(HttpStatus.NOT_FOUND));
+    Mockito.when(caseService.getCCSCaseByPostcode(eq("GW12 AAB"))).thenThrow(ex);
+    ResultActions actions = mockMvc.perform(getJson("/cases/ccs/postcode/GW12 AAB"));
+    actions.andExpect(status().isNotFound());
+  }
 
   @Test
   public void getCcsCaseByPostcode_PostcodeNotInList() throws Exception {
@@ -104,97 +106,11 @@ public final class CaseEndpointGetCcsCaseTest {
             HttpStatus.BAD_REQUEST,
             "Bad Request",
             new HttpClientErrorException(HttpStatus.BAD_REQUEST));
-    //    Mockito.doThrow(ex).when(caseService.getCCSCaseByPostcode(eq("GW12 AAC")));
     Mockito.when(caseService.getCCSCaseByPostcode(eq("GW12 AAC"))).thenThrow(ex);
     ResultActions actions = mockMvc.perform(getJson("/cases/ccs/postcode/GW12 AAC"));
     actions.andExpect(status().isBadRequest());
   }
 
-  //
-  // @Test
-  // public void getCaseById_CaseEventsTrue() throws Exception {
-  // CaseDTO testCaseDTO = createResponseCaseDTO();
-  // Mockito.when(caseService.getCaseById(eq(uuid), any())).thenReturn(testCaseDTO);
-  //
-  // ResultActions actions = mockMvc.perform(getJson("/cases/" + uuid + "?caseEvents=1"));
-  // actions.andExpect(status().isOk());
-  //
-  // verifyStructureOfResultsActions(actions);
-  // }
-  //
-  // @Test
-  // public void getCaseById_CaseEventsDuff() throws Exception {
-  // ResultActions actions = mockMvc.perform(getJson("/cases/" + uuid + "?caseEvents=maybe"));
-  // actions.andExpect(status().isBadRequest());
-  // }
-  //
-  // @Test
-  // public void getCaseByRef_GoodRef() throws Exception {
-  // CaseDTO testCaseDTO = createResponseCaseDTO();
-  // Mockito.when(caseService.getCaseByCaseReference(eq(123456L),
-  // any())).thenReturn(testCaseDTO);
-  //
-  // ResultActions actions = mockMvc.perform(getJson("/cases/ref/123456"));
-  // actions.andExpect(status().isOk());
-  //
-  // verifyStructureOfResultsActions(actions);
-  // }
-  //
-  // @Test
-  // public void getCaseByRef_BadRef() throws Exception {
-  // ResultActions actions = mockMvc.perform(getJson("/cases/ref/avg"));
-  // actions.andExpect(status().isBadRequest());
-  // }
-  //
-  // @Test
-  // public void getCaseByUprn_GoodUPRN() throws Exception {
-  // List<CaseDTO> testCases = new ArrayList<>();
-  // testCases.add(createResponseCaseDTO());
-  // testCases.add(createResponseCaseDTO());
-  // UniquePropertyReferenceNumber expectedUprn = new
-  // UniquePropertyReferenceNumber(123456789012L);
-  // Mockito.when(caseService.getCaseByUPRN(eq(expectedUprn), any())).thenReturn(testCases);
-  //
-  // ResultActions actions = mockMvc.perform(getJson("/cases/uprn/123456789012"));
-  // actions.andExpect(status().isOk());
-  //
-  // verifyStructureOfMultiResultsActions(actions);
-  // }
-  //
-  // @Test
-  // public void getCaseByUprn_UPRNTooLong() throws Exception {
-  // ResultActions actions = mockMvc.perform(getJson("/cases/uprn/123456789012345"));
-  // actions.andExpect(status().isBadRequest());
-  // }
-  //
-  // @Test
-  // public void getCaseByUprn_BadUPRN() throws Exception {
-  // ResultActions actions = mockMvc.perform(getJson("/cases/uprn/A12345678901234"));
-  // actions.andExpect(status().isBadRequest());
-  // }
-  //
-  // @Test
-  // public void getCaseByUprn_CaseEventsTrue() throws Exception {
-  // List<CaseDTO> testCases = new ArrayList<>();
-  // testCases.add(createResponseCaseDTO());
-  // testCases.add(createResponseCaseDTO());
-  // UniquePropertyReferenceNumber expectedUprn = new
-  // UniquePropertyReferenceNumber(123456789012L);
-  // Mockito.when(caseService.getCaseByUPRN(eq(expectedUprn), any())).thenReturn(testCases);
-  //
-  // ResultActions actions = mockMvc.perform(getJson("/cases/uprn/123456789012?caseEvents=1"));
-  // actions.andExpect(status().isOk());
-  //
-  // verifyStructureOfMultiResultsActions(actions);
-  // }
-  //
-  // @Test
-  // public void getCaseByUprn_CaseEventsDuff() throws Exception {
-  // ResultActions actions =
-  // mockMvc.perform(getJson("/cases/uprn/12345678901234?caseEvents=maybe"));
-  // actions.andExpect(status().isBadRequest());
-  // }
-  //
   private CaseDTO createResponseCaseDTO() throws ParseException {
     SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSX");
 
@@ -224,27 +140,7 @@ public final class CaseEndpointGetCcsCaseTest {
     return fakeCaseDTO;
   }
 
-  // private void verifyStructureOfResultsActions(ResultActions actions) throws Exception {
-  // actions.andExpect(jsonPath("$[0].id", is(CASE_UUID_STRING)));
-  // actions.andExpect(jsonPath("$[0].caseRef", is(CASE_REF)));
-  // actions.andExpect(jsonPath("$[0].caseType", is(CASE_TYPE)));
-  // actions.andExpect(jsonPath("$[0].createdDateTime", is(CASE_CREATED_DATE_TIME)));
-  // actions.andExpect(jsonPath("$[0].addressLine1", is(ADDRESS_LINE_1)));
-  // actions.andExpect(jsonPath("$[0].addressLine2", is(ADDRESS_LINE_2)));
-  // actions.andExpect(jsonPath("$[0].addressLine3", is(ADDRESS_LINE_3)));
-  // actions.andExpect(jsonPath("$[0].townName", is(TOWN)));
-  // actions.andExpect(jsonPath("$[0].region", is(REGION)));
-  // actions.andExpect(jsonPath("$[0].postcode", is(POSTCODE)));
-  // actions.andExpect(jsonPath("$[0].ceOrgName", is(ORG_NAME)));
-  //
-  // actions.andExpect(jsonPath("$[0].caseEvents[0].category", is(EVENT_CATEGORY)));
-  // actions.andExpect(jsonPath("$[0].caseEvents[0].description", is(EVENT_DESCRIPTION)));
-  // actions.andExpect(jsonPath("$[0].caseEvents[0].createdDateTime", is(EVENT_DATE_TIME)));
-  // }
-
   private void verifyStructureOfMultiResultsActions(ResultActions actions) throws Exception {
-    // This is not ideal - obvious duplication here - want to find a neater way of making the same
-    // assertions repeatedly
     actions.andExpect(jsonPath("$[0].id", is(CASE_UUID_STRING)));
     actions.andExpect(jsonPath("$[0].caseRef", is(CASE_REF)));
     actions.andExpect(jsonPath("$[0].caseType", is(CASE_TYPE)));
